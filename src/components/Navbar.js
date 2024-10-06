f15312cd4eaabef88f78d50a0d3382fb6b3e6970
adilabbasi-786 <m.adilabbasi786@gmail.com>
2024-10-06 12:32:27 +0500

    set toggle menu for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,12 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 bg-white border-b border-gray-200 w-[1512] h-[86] dark:bg-neutral-800 dark:border-neutral-700">
+    <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 bg-white border-b border-gray-200 w-full h-[86px] dark:bg-neutral-800 dark:border-neutral-700">
       <nav className="relative max-w-[85rem] w-full mx-auto md:flex md:items-center md:justify-between md:gap-3 py-2 px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center gap-x-1">
           <a
@@ -22,7 +26,7 @@ const Navbar = () => {
 
           <button
             type="button"
-            className="hs-collapse-toggle md:hidden relative size-9 flex justify-center items-center font-medium text-[12px] rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+            className="md:hidden relative size-9 flex justify-center items-center font-medium text-[12px] rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
             onClick={toggleMenu}
             aria-expanded={isOpen ? "true" : "false"}
             aria-controls="hs-header-base"
@@ -68,42 +72,47 @@ const Navbar = () => {
         <div
           id="hs-header-base"
           className={`hs-collapse overflow-hidden transition-all duration-300 ${
-            isOpen ? "block" : "hidden"
-          } basis-full grow md:block`}
+            isOpen ? "fixed inset-0 w-full h-full bg-white z-40" : "hidden"
+          } md:block md:relative md:bg-transparent md:inset-auto md:h-auto md:overflow-visible`}
           aria-labelledby="hs-header-base-collapse"
         >
-          <div className="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
+          <div className="overflow-hidden overflow-y-auto max-h-[75vh] md:max-h-none">
             <div className="py-2 md:py-0 flex flex-col md:flex-row md:items-center gap-5 md:gap-1">
               <div className="grow">
                 <div className="flex flex-col md:flex-row md:justify-end md:items-center md:gap-1">
                   <a
-                    className="p-2 flex items-center text-[#14A09D] hover:text-[#14A09D] focus:text-[#14A09D] rounded-lg focus:outline-none font-poppins text-sm font-normal leading-5 text-left"
+                    className="p-4 text-[#14A09D] hover:text-[#14A09D] focus:text-[#14A09D] rounded-lg font-poppins text-sm font-normal leading-5 text-left"
                     href="/"
                     aria-current="page"
+                    onClick={closeMenu}
                   >
                     Home
                   </a>
                   <a
-                    className="p-2 flex items-center text-[#28283C] hover:text-[#14A09D] focus:text-[#14A09D] rounded-lg focus:outline-none font-poppins text-sm font-normal leading-5 text-left"
+                    className="p-4 text-[#28283C] hover:text-[#14A09D] focus:text-[#14A09D] rounded-lg font-poppins text-sm font-normal leading-5 text-left"
                     href="#"
+                    onClick={closeMenu}
                   >
                     News
                   </a>
                   <a
-                    className="p-2 flex items-center text-[#28283C] hover:text-[#14A09D] focus:text-[#14A09D] rounded-lg focus:outline-none font-poppins text-sm font-normal leading-5 text-left"
+                    className="p-4 text-[#28283C] hover:text-[#14A09D] focus:text-[#14A09D] rounded-lg font-poppins text-sm font-normal leading-5 text-left"
                     href="#"
+                    onClick={closeMenu}
                   >
                     Research
                   </a>
                   <a
-                    className="p-2 flex items-center text-[#28283C] hover:text-[#14A09D] focus:text-[#14A09D] rounded-lg focus:outline-none font-poppins text-sm font-normal leading-5 text-left"
+                    className="p-4 text-[#28283C] hover:text-[#14A09D] focus:text-[#14A09D] rounded-lg font-poppins text-sm font-normal leading-5 text-left"
                     href="#"
+                    onClick={closeMenu}
                   >
                     Contact
                   </a>
                   <a
-                    className="p-2 flex items-center text-[#28283C] hover:text-[#14A09D] focus:text-[#14A09D] rounded-lg focus:outline-none font-poppins text-sm font-normal leading-5 text-left"
+                    className="p-4 text-[#28283C] hover:text-[#14A09D] focus:text-[#14A09D] rounded-lg font-poppins text-sm font-normal leading-5 text-left"
                     href="#"
+                    onClick={closeMenu}
                   >
                     FAQ
                   </a>
@@ -111,14 +120,14 @@ const Navbar = () => {
               </div>
 
               <div className="flex flex-wrap items-center gap-x-1.5 ml-5">
-                <Link to="/login">
-                  <button className="w-[140px] h-[40px] min-w-[140px] p-x-4 gap-x-[8px] rounded-full border border-solid border-[#14A09D] hover:bg-[#14A09D]  group ">
+                <Link to="/login" onClick={closeMenu}>
+                  <button className="w-[140px] h-[40px] min-w-[140px] px-4 gap-x-2 rounded-full border border-solid border-[#14A09D] hover:bg-[#14A09D] group">
                     <p className="font-poppins text-sm font-normal leading-7 text-center text-[#14A09D] group-hover:text-white">
                       Login
                     </p>
                   </button>
                 </Link>
-                <button className="w-[140px] h-[40px] min-w-[140px] p-x-4 gap-x-[8px] rounded-full border border-solid border-[#14A09D] bg-[#14A09D] hover:bg-[#0E8F8D]">
+                <button className="w-[140px] h-[40px] min-w-[140px] px-4 gap-x-2 rounded-full border border-solid border-[#14A09D] bg-[#14A09D] hover:bg-[#0E8F8D]">
                   <p className="font-poppins text-sm font-normal leading-7 text-center text-[#FFFFFF]">
                     Get Started
                   </p>
